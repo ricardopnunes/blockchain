add6d346bda2f3a23a2976ab4d55df2b4cc66095
ricardopnunes <ricardopontin145@gmail.com>
2024-03-01 09:05:04 -0300

    add: First Commit

---
+++ src/block.rs
@@ -0,0 +1,13 @@
+pub struct Block{
+    timestamp: u128,
+    transactions: String,
+    perv_block_hash: String,
+    hash: String,
+    height: usize,
+    nonce: i32,
+}
+
+pub struct Blockchain {
+
+
+}
